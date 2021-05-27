66b56c3fa0144f30999ec68fe436169f911d25e5
thuy-nguen <84868012+thuy-nguen@users.noreply.github.com>
2021-05-27 13:05:18 +0700

    Add files via upload

---
+++ untitled8.py
@@ -0,0 +1,59 @@
+
+
+import numpy as np
+import cv2 
+import glob
+import cv2_
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((7*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images=  glob.glob('C:/Users/VTS/source/repos/gk/gk')
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7,7), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (7,7), corners2, ret)
+        cv2_imshow(img)
+        cv2.waitKey(500)
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+img=  cv2.imread('C:/Users/VTS/source/repos/gk/gk/bancow16')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+print(mtx)
+print(rvecs)
+print(tvecs)
+
+# undistort
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('calibresult.png', dst)
+cv2.imshow(img)
+tot_error=0
+total_points=0
+for i in range(len(imgpoints)):
+    reprojected_points =
+    reprojected_points=reprojected_points.reshape(-1,2)
+    tot_error+=np.sum(np.abs(objpoints [i]-reprojected_points)**2)
+    total_points+=len(imgpoints[i])
+
+mean_error=np.sqrt(tot_error/total_points)
+print("Mean reprojection error:", mean_error)
